7606368fac12a08c2ea12076ac57d03bdad77910
pedriyn <jc7633505@gmail.com>
2026-09-03 00:21:21 +0000

    first commit

---
+++ world.py
@@ -0,0 +1,12 @@
+def saudacao(nome):
+    print("\n" + "=" * 40)
+    print(f"Olá, {nome}!")
+    print("Seja bem-vindo ao mundo da programação!")
+    print("=" * 40)
+
+
+print("HELLO WORLD")
+
+nome = input("Digite seu nome: ")
+
+saudacao(nome)
